9bb98666bc2c48115950acb21a9c473db98ef53e
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-12-04 19:16:34 +0530

    upload files

---
+++ FrontEnd/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { UploadService } from './upload.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // @ts-ignore
+  file: File = null;
+
+  constructor(
+    private uploadService: UploadService
+  ){
+
+  }
+
+  onFilechange(event: any) {
+    console.log(event.target.files[0])
+    this.file = event.target.files[0]
+  }
+
+  upload() {
+    if (this.file) {
+      this.uploadService.uploadfile(this.file).subscribe(resp => {
+        alert("Uploaded")
+      })
+    } else {
+      alert("Please select a file first")
+    }
+  }
+}
